e24b8398d2efb521fc1b7e35c37ab5f1c22df887
gloyerasamkto <gloyer777@gmail.com>
2026-04-22 00:49:09 +0300

    build 1.1, first bug fix update

---
+++ 1.20.1_fabric/src/main/java/org/gloyer057/cuffsx/network/CuffRemovePayload.java
@@ -0,0 +1,10 @@
+package org.gloyer057.cuffsx.network;
+
+import net.minecraft.util.Identifier;
+
+/**
+ * Пакет снятия наручников с указанием типа (HANDS/LEGS).
+ */
+public class CuffRemovePayload {
+    public static final Identifier ID = new Identifier("cuffsx", "cuff_remove");
+}
